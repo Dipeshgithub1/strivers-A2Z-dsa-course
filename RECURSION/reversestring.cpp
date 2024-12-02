7d153d27df0dd986f91d18c24372cbd73324778a
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2024-12-02 20:17:19 +0530

    Recursion update

---
+++ RECURSION/reversestring.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void reverse(string& str,int i,int j){
+    //base case 
+    if(i> j){
+        return;
+       
+    }
+     swap(str[i],str[j]);
+        i++;
+        j--;
+        reverse(str,i,j);
+    
+}
+
+int main() {
+    string name = "babber";
+    reverse(name,0,name.length()-1);
+    cout << name << endl;
+
+    return 0;
+}

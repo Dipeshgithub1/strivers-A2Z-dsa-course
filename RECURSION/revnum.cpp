fc8a44581211545f50e374182fc674b91f9c4f55
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2024-12-10 23:25:06 +0530

    Bit Manupulation  update

--- RECURSION/revnum.cpp
+++ RECURSION/revnum.cpp
@@ -20,3 +20,25 @@ int main()
  
     cout << (power(x, n));
 }
+
+//2. Count of numbers (x) smaller than or equal to n such that n+x = n^x:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countValue(int n){
+    int unset_bits = 0;
+    while(n){
+        if((n&1) == 0)
+        unset_bits++;
+        n=n>>1;
+    }
+    return 1 << unset_bits;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout<< "count Value  : " <<countValue(n);
+    return 0;
+}

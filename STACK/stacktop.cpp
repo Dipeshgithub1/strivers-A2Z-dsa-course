c1238d520d8f70d9df9c6e67a98e11ecfbe16ce6
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2025-01-17 22:24:12 +0530

    STACK

---
+++ STACK/stacktop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<int> st;
+    st.push(5);
+    st.push(11);
+      
+      cout << st.top() << endl;
+  
+    st.push(9);
+      cout << st.top();
+    return 0;
+}

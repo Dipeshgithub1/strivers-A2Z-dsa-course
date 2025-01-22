36b0e136b585e08e7533f20edff5541baac75655
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2025-01-22 22:16:04 +0530

    Array

---
+++ ARRAY/secondlargest.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int getSecondLarge(vector<int> &arr){
+    int n = arr.size();
+    
+    sort(arr.begin(), arr.end());
+    
+    for(int i = n-2; i>=0;i--){
+        if(arr[i] != arr[n-1]){
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {12,35,1,10,34,1};
+    cout<< getSecondLarge(arr);
+
+    return 0;
+}

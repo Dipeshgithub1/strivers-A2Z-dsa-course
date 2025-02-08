6adbf5eb63f9d9a94c30a2e8b61a4ef356274dfe
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2025-02-08 23:46:49 +0530

    recursion revise

--- RECURSION/revise/recursion.cpp
+++ RECURSION/revise/recursion.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 /*
 int sum(int n){
@@ -7,16 +7,21 @@ int sum(int n){
     }
     return n + sum(n-1);
 }
-int main(){
-int n;
-cout<<"enter is number : ";
-cin >> n;
-cout<<"enter number is : "<<n <<endl;
 
-cout <<"sum of first "<< n  <<" natural number is : " <<sum(n) << endl;;
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin >> n;
+    cout<<"sum of first "<<n<< " is : ";
+    cout<<sum(n);
+
+
 }
 
+*/
+
 int fact(int n){
+    //base case
     if(n==0){
         return 1;
     }
@@ -24,49 +29,10 @@ int fact(int n){
 }
 
 int main(){
-int n;
-cout<<"Enter number : ";
-cin >> n;
-cout<<"Enter number is : "<<n <<endl;
-cout <<"fact of first of  " << n << " factorial number is : " << fact(n) << endl;
-
-}
-
-
-void printTest(int test){
- if(test < 1){
-    return;
- }
- else{
-    cout << test << " ";
-    printTest(test-1);
-    cout << test << " ";
-    return;
- }
-}
-
-int main(){
-int test =3;
-printTest(test);
-}
-*/
-int fibo(int n){
-    if(n == 0){
-        return 0;
-    }
-    else if( n==1 || n==2){
-        return 1;
-    }
-    return fibo(n-1) + fibo(n-2);
-}
-
-int main(){
-int n;
-cout <<"Enter number is : ";
-cin >> n;
-cout<<"Fibonacci series of 5 numbers is: ";
-for(int i=0;i<n;i++){
-cout<<fibo(i)<< " ";
-}
+    int n;
+    cout<<"Enter a number : ";
+    cin >> n;
+    cout<<"factorial of first  "<<n<< " is : ";
+    cout<<fact(n);    
 
 }

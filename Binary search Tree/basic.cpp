d120c38daede2a41f59778757b18726fc609a18c
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2025-02-09 16:49:10 +0530

    recursion revise

---
+++ Binary search Tree/basic.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+      int data;
+      node* left;
+      node* right;
+      
+      node(int d){
+          this -> data = d;
+          this -> left = NULL;
+          this -> right = NULL;
+      }
+};
+node* buildTree(node* root){
+    cout<< "Enter the data : "<<endl;
+    int data;
+    cin>> data;
+    root = new node(data);
+    if(data == -1){
+        return NULL;
+    }
+    cout<<"Enter data for inserting in left "<<endl;
+    root->left = buildTree(root->left);
+    cout<<"Enter data for inserting in right " <<endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+
+int main(){
+    node* root = NULL;
+    
+    //creating a tree
+    root = buildTree(root);
+}
